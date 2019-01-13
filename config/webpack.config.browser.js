4975a7fec7a40aa6cbe7d99bece952969ec3f382
shiqwang <shiqwang@gmail.com>
2019-01-14 01:55:35 +0800

    init

---
+++ config/webpack.config.browser.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack')
+var merge = require('webpack-merge')
+var base = require('./webpack.config.base')
+
+var outputFile = 'someline-ui'
+var globalName = 'SomelineUI'
+
+module.exports = merge(base, {
+  output: {
+    path: './dist',
+    filename: outputFile + '.browser.js',
+    library: globalName,
+    libraryTarget: 'umd',
+  },
+  externals: {
+    // Put external libraries like lodash here
+    // With their global name
+    // Example: 'lodash': '_'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: true,
+      },
+      mangle: false,
+    }),
+  ],
+})
